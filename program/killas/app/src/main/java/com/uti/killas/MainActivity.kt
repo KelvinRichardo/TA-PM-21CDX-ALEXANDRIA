16ce7840636a3f4eed80d5c08a73afa6da5dd0b1
KelvinRichardo <kelvinrichardo86@gmail.com>
2024-06-25 18:19:01 +0700

    master: Update

--- program/killas/app/src/main/java/com/uti/killas/MainActivity.kt
+++ program/killas/app/src/main/java/com/uti/killas/MainActivity.kt
@@ -1,41 +1,31 @@
 package com.uti.killas
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import com.uti.killas.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    lateinit var username : EditText
-    lateinit var password: EditText
-    lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.loginButton.setOnClickListener(View.OnClickListener {
-
-            if (binding.username.text.toString() == "user" && binding.password.text.toString() == "1234"){
-                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
-
-            val loginButton = findViewById<Button>(R.id.loginButton)
-                loginButton.setOnClickListener {
-                    val intent = setIntent(this,BioActivity::class.java)
-                    startActivity(intent)
-                }
-
-            }
-        })
+        val loginButton: Button = findViewById(R.id.loginButton)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, BioActivity::class.java)
+            startActivity(intent)
 
 
+        }
+        fun TampilkanBiodata(view: View) {
+            val intent = Intent(this, BioActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
+
