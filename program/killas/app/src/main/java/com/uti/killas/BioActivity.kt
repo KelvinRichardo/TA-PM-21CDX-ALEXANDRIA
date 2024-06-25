524ffa74fb415816b493b0de2a4b72cc2efdf3dc
KelvinRichardo <kelvinrichardo86@gmail.com>
2024-06-25 19:55:24 +0700

    dokumentasi kedua

--- program/killas/app/src/main/java/com/uti/killas/BioActivity.kt
+++ program/killas/app/src/main/java/com/uti/killas/BioActivity.kt
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 
 class BioActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_biodata)
 
+        val MasukButton: Button = findViewById(R.id.MasukButton)
+        MasukButton.setOnClickListener {
+            val intent = Intent(this, RoomActivity::class.java)
+            startActivity(intent)
+    }
 
-    }}
+    fun tampilkanRoom(view: View) {
+        val intent = Intent(this, RoomActivity::class.java)
+        startActivity(intent)
+
+}
