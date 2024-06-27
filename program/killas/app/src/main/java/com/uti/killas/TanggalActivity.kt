972f18607b0c99d32d2f7580800da9629dcf4b76
KelvinRichardo <kelvinrichardo86@gmail.com>
2024-06-28 05:55:32 +0700

    iyeti:meneruskan activity tanggal

---
+++ program/killas/app/src/main/java/com/uti/killas/TanggalActivity.kt
@@ -0,0 +1,17 @@
+package com.uti.killas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+
+class TanggalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tanggal)
+
+    }
+}
