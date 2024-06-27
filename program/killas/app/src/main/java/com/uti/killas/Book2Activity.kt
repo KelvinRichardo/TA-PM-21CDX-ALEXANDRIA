dd330aad00b62e3f8f0705d8018aa9997e189a46
KelvinRichardo <kelvinrichardo86@gmail.com>
2024-06-28 06:43:42 +0700

    iyeti:pembaruan

--- program/killas/app/src/main/java/com/uti/killas/Book2Activity.kt
+++ program/killas/app/src/main/java/com/uti/killas/Book2Activity.kt
@@ -3,7 +3,6 @@ package com.uti.killas
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 
 class Book2Activity : AppCompatActivity() {
@@ -11,5 +10,11 @@ class Book2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book2)
+
+        val jwl_2: Button = findViewById(R.id.jwl_2)
+        jwl_2.setOnClickListener {
+            val intent = Intent(this, TanggalActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- program/killas/app/src/main/java/com/uti/killas/Book3Activity.kt
+++ program/killas/app/src/main/java/com/uti/killas/Book3Activity.kt
@@ -11,5 +11,11 @@ class Book3Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book3)
+
+        val jwl_3: Button = findViewById(R.id.jwl_3)
+        jwl_3.setOnClickListener {
+            val intent = Intent(this, TanggalActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
