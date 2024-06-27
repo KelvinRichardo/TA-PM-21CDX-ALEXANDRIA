8274e44baf39689be1e0b1df669ff438866bd831
KelvinRichardo <kelvinrichardo86@gmail.com>
2024-06-28 06:41:53 +0700

    iyeti:fix

--- program/killas/app/src/main/java/com/uti/killas/BookActivity.kt
+++ program/killas/app/src/main/java/com/uti/killas/BookActivity.kt
@@ -11,5 +11,11 @@ class BookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book)
+
+        val jwl_1: Button = findViewById(R.id.jwl_1)
+        jwl_1.setOnClickListener {
+            val intent = Intent(this, TanggalActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
